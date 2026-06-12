db697c6a7c9a2537dce083146a9a2f4c376873a2
joao gustavo <j.gustavo.s.v.197@gmail.com>
2026-06-12 15:01:23 -0300

    Aula dia 12/06/26

---
+++ Logica-Progamacao/variaveis.py
@@ -0,0 +1,61 @@
+nome = "Gustavo"
+idade = 25
+altura = 1.75
+tem_mundial = False        
+
+print(nome)
+print(idade)
+print(altura)
+print(tem_mundial)
+
+
+joao = "Joao"
+comida = "Pao"
+partido = "Direita"
+jogador = "Neymar"
+time = "Santos"
+
+print(joao)
+print(comida)
+print(partido)
+print(jogador)
+print(time)
+
+ano = 1970
+cpf = 12345678910
+senha = 777888
+telefone = 99999999999
+rg = 676767676767
+
+print(ano)
+print(cpf)
+print(senha)
+print(telefone)
+print(rg)
+
+saldo = 50.00
+kg = 4.5
+km = 10.0
+altura = 1.75
+horario = 10.40
+
+print(saldo)
+print(kg)
+print(km)
+print(altura)
+print(horario)
+
+passou_de_ano = True
+vc_farma_aura = True
+tem_libertadores = True
+santos_e_maior_time_do_mundo = True 
+tem_dinheiro = False
+
+print(passou_de_ano)
+print(vc_farma_aura)
+print(tem_libertadores)
+print(santos_e_maior_time_do_mundo)
+print(tem_dinheiro)
+
+
+
